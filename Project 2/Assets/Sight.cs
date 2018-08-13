b498797163e694534926840cbc4d4e99831ad863
Alan Eir <40726675+QwaserLi@users.noreply.github.com>
2018-08-13 21:55:37 +1200

    Right Project
    The right unity project

---
+++ Project 2/Assets/Sight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //var ray1 = new Ray(transform.position, transform.forward);
+
+        //var hit = 
+        //var int_HitRadiusDistance = 20;
+
+        //if (Physics.Raycast(ray1, hit, int_HitRadiusDistance))
+        //{
+        //    if ((hit.collider.name == "FPS") || (hit.collider.name == "Player"))
+        //    {
+        //        // TODO: Attack-code   
+        //    }
+        //}
+    }
+}
