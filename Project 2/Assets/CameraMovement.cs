02c3b8b2338aa34b1585011b000952a8af33f81d
QwaserLi <40726675+QwaserLi@users.noreply.github.com>
2018-08-13 20:40:24 +1200

    Intial Commit

---
+++ Project 2/Assets/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement * 1 * Time.deltaTime, Space.World);
+
+    }
+}
